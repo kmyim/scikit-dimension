9401079a48a8768c0bb6d2b77a80187b62293635
johnharveymath <john.harvey.math@gmail.com>
2024-03-18 12:19:13 +0000

    Equalise aspect ratio

--- examples/plot_dataset.py
+++ examples/plot_dataset.py
@@ -7,10 +7,11 @@ Code to quickly visualise a datset
 import skdim
 import matplotlib.pyplot as plt
 
-dataset = skdim.datasets.hyperSphere(n=1000, d=3, radius=1)
+dataset = skdim.datasets.toroidal_spiral(n=1000, n_twists=25)
 
 fig = plt.figure(figsize=(10, 5))
 ax = fig.add_subplot(projection='3d')
 ax.scatter(dataset[:, 0], dataset[:, 1], dataset[:, 2])
+ax.axis('equal')
 ax.set_title("First three dimensions of dataset")
 plt.show()
