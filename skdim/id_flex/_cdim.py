9f96b027ec7aabd0efdafe3b3214136a47ddd746
johnharveymath <john.harvey.math@gmail.com>
2024-06-14 16:46:47 +0100

    Linting

--- skdim/id_flex/_cdim.py
+++ skdim/id_flex/_cdim.py
@@ -1,21 +1,33 @@
 import numpy as np
 from .._commonfuncs import FlexNbhdEstimator
 
+
 class CDim(FlexNbhdEstimator):
-    '''
+    """
     Conical dimension method with either knn or epsilon neighbourhood
 
     Yang et al.
     Conical dimension as an intrinsic dimension estimator and its applications
     https://doi.org/10.1137/1.9781611972771.16
-    '''
+    """
 
+    def _fit(
+        self,
+        X,
+        nbhd_indices,
+        nbhd_type,
+        metric,
+        radial_dists,
+        radius=1.0,
+        n_neighbors=5,
+    ):
 
-    def _fit(self, X, nbhd_indices, nbhd_type, metric, radial_dists, radius = 1.0, n_neighbors = 5,):
+        if nbhd_type not in ["eps", "knn"]:
+            raise ValueError("Neighbourhood type should either be knn or eps.")
 
-        if nbhd_type not in ['eps', 'knn']: raise ValueError('Neighbourhood type should either be knn or eps.')
-
-        self.dimension_pw_ = np.array([self._local_cdim(X, idx, nbhd) for idx, nbhd in enumerate(nbhd_indices)])
+        self.dimension_pw_ = np.array(
+            [self._local_cdim(X, idx, nbhd) for idx, nbhd in enumerate(nbhd_indices)]
+        )
 
     @staticmethod
     def _local_cdim(X, idx, nbhd):
--- skdim/tests/flex/cdim_test.py
+++ skdim/tests/flex/cdim_test.py
@@ -4,17 +4,19 @@ import skdim.id_flex
 from sklearn.datasets import make_swiss_roll
 import skdim.errors
 
+
 def test_on_swiss_roll_knn():
     np.random.seed(782)
     swiss_roll_dat = make_swiss_roll(1000)
     cdim = skdim.id_flex.CDim()
-    estim_dim = cdim.fit_transform(swiss_roll_dat[0], nbhd_type = 'knn', n_neighbors=10)
+    estim_dim = cdim.fit_transform(swiss_roll_dat[0], nbhd_type="knn", n_neighbors=10)
     # The expected value is taken from Yang et al
     assert 2.0 == pytest.approx(estim_dim, 0.02)
+
 
 def test_on_swiss_roll_eps():
     np.random.seed(782)
     swiss_roll_dat = make_swiss_roll(1000)
     cdim = skdim.id_flex.CDim()
-    estim_dim = cdim.fit_transform(swiss_roll_dat[0], nbhd_type = 'eps', radius=3.0)
+    estim_dim = cdim.fit_transform(swiss_roll_dat[0], nbhd_type="eps", radius=3.0)
     assert 2.0 == pytest.approx(estim_dim, 0.02)
