e85209e506677d931cdc52f52620c8cd6c838d81
johnharveymath <john.harvey.math@gmail.com>
2024-06-14 16:45:30 +0100

    Working for eps

--- skdim/id_flex/_cdim.py
+++ skdim/id_flex/_cdim.py
@@ -13,25 +13,27 @@ class CDim(FlexNbhdEstimator):
 
     def _fit(self, X, nbhd_indices, nbhd_type, metric, radial_dists, radius = 1.0, n_neighbors = 5,):
 
-        if nbhd_type not in ['knn']: raise ValueError('Neighbourhood type should be knn.')
+        if nbhd_type not in ['eps', 'knn']: raise ValueError('Neighbourhood type should either be knn or eps.')
 
-        self.dimension_pw_ = np.array([self._local_cdim(X, idx, nbhd, n_neighbors) for idx, nbhd in enumerate(nbhd_indices)])
+        self.dimension_pw_ = np.array([self._local_cdim(X, idx, nbhd) for idx, nbhd in enumerate(nbhd_indices)])
 
     @staticmethod
-    def _local_cdim(X, idx, nbhd, n_neighbors):
+    def _local_cdim(X, idx, nbhd):
+        if len(nbhd) == 0:
+            return 0
         neighbors = np.take(X, nbhd, 0)
         vectors = neighbors - X[idx]
         norms = np.linalg.norm(vectors, axis=1)
         vectors = np.take(vectors, np.argsort(norms), axis=0)
         vectors = vectors * np.sign(np.tensordot(vectors, vectors[0], axes=1))[:, None]
         T = np.matmul(vectors, vectors.T)
-        label_set = [[i] for i in range(n_neighbors)]
+        label_set = [[i] for i, _ in enumerate(nbhd)]
         de = 0
         while len(label_set) > 0:
             de += 1
             new_label_set = []
             for label in label_set:
-                for i in range(n_neighbors):
+                for i, _ in enumerate(nbhd):
                     if i in label:
                         continue
                     for vec_idx in label:
