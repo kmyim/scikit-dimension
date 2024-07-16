af64be7d8e966ada6cdf7d7cc2c74ba1b39f32db
johnharveymath <john.harvey.math@gmail.com>
2024-07-16 14:32:58 +0100

    Passing all tests

--- skdim/tests/flex/pca_test.py
+++ skdim/tests/flex/pca_test.py
@@ -15,25 +15,24 @@ def data():
 
 @pytest.mark.parametrize("ver", VERSIONS)
 def test_lpca_results(data, ver):
-    assert skdim.id_flex.lPCA(ver=ver).fit(
-        data, nbhd_type="eps", radius=0.5
-    ).dimension_ == pytest.approx(5, 0.5)
+    pca = skdim.id_flex.lPCA(ver=ver, nbhd_type="eps", radius=0.5)
+    assert pca.fit(data).dimension_ == pytest.approx(5, 0.5)
 
 
 @pytest.mark.parametrize("ver", VERSIONS)
 def test_on_swiss_roll(ver):
     np.random.seed(782)
     swiss_roll_dat = make_swiss_roll(1000)
-    pca = skdim.id_flex.lPCA(ver=ver)
-    estim_dim = pca.fit_transform(swiss_roll_dat[0], nbhd_type="knn", n_neighbors=20)
+    pca = skdim.id_flex.lPCA(ver=ver, nbhd_type="knn", n_neighbors=20)
+    estim_dim = pca.fit_transform(swiss_roll_dat[0])
     assert 2.0 == pytest.approx(estim_dim, 0.5)
 
 
 def test_when_eps_and_knn_almost_equivalent():
-    pca = skdim.id_flex.lPCA()
+    pca = skdim.id_flex.lPCA
     rectangle = np.zeros((4, 4))
     rectangle[1:3, 1] = 2
     rectangle[:2, 0] = 1
-    knn_estim = pca.fit_transform(rectangle, nbhd_type="knn", n_neighbors=2)
-    eps_estim = pca.fit_transform(rectangle, nbhd_type="eps", radius=2)
+    knn_estim = pca(nbhd_type="knn", n_neighbors=2).fit_transform(rectangle)
+    eps_estim = pca(nbhd_type="eps", radius=2).fit_transform(rectangle)
     assert knn_estim == pytest.approx(eps_estim)
