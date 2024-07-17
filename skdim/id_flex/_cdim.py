89b163da163a87c112a7e5a80ac57a75eb2e343c
johnharveymath <john.harvey.math@gmail.com>
2024-07-17 12:19:15 +0100

    Correct __init__ parameters

--- skdim/id_flex/_cdim.py
+++ skdim/id_flex/_cdim.py
@@ -13,6 +13,39 @@ class CDim(FlexNbhdEstimator):
     https://doi.org/10.1137/1.9781611972771.16
     """
 
+    def __init__(
+        self,
+        nbhd_type="knn",
+        metric="euclidean",
+        comb="mean",
+        smooth=False,
+        n_jobs=1,
+        radius=1.0,
+        n_neighbors=5,
+    ):
+        """
+        nbhd_type: either 'knn' (k nearest neighbour) or 'eps' (eps nearest neighbour) or 'custom'
+        metric: defaults to standard euclidean metric; if X a distance matrix, then set to 'precomputed'
+        comb: method of averaging either 'mean', 'median', or 'hmean'
+        smooth: if true, average over dimension estimates of local neighbourhoods using comb
+        n_jobs: number of parallel processes in inferring local neighbourhood
+        radius: radius parameter for nearest neighbour construction
+        n_neighbors: number of neighbors for k nearest neighbourhood construction
+        """
+
+        super().__init__(
+            pw_dim=True,
+            nbhd_type=nbhd_type,
+            pt_nbhd_incl_pt=False,
+            metric=metric,
+            comb=comb,
+            smooth=smooth,
+            n_jobs=n_jobs,
+            radius=radius,
+            n_neighbors=n_neighbors,
+            sort_radial=False,
+        )
+
     def _fit(self, X, nbhd_indices, radial_dists):
 
         if effective_n_jobs(self.n_jobs) > 1:
