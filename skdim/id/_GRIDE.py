358ba7825057376fb122c2791771a1f85e5f4e6e
Jakub <jakubbmalinowski@gmail.com>
2024-10-28 20:17:58 +0100

    Add multiplier param
    This commit adds optional multiplier param to GRIDE constructor.
    It simplifies experiments.

--- skdim/id/_GRIDE.py
+++ skdim/id/_GRIDE.py
@@ -63,7 +63,7 @@ class Gride(GlobalEstimator):
             estimators, arXiv preprint arXiv:2104.13832 (2021).
     """
     
-    def __init__(self, n1=1, n2=None, d0=0.001, d1=1000, eps=1e-7, range_max=None, metric="euclidean", n_jobs=1):
+    def __init__(self, n1=1, n2=None, d0=0.001, d1=1000, eps=1e-7, range_max=None, metric="euclidean", n_jobs=1, multiplier = 2):
         """Initialize the GRIDE object.
         Parameters
         ----------
@@ -85,7 +85,7 @@ class Gride(GlobalEstimator):
         self.n_jobs = n_jobs
         self.n1 = n1
         if n2 == None:
-            self.n2 = 2 * n1
+            self.n2 = multiplier * n1
         else:
             self.n2 = n2
         self.d0 = d0
