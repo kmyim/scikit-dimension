15ce81d7824d8e9314f811a34cf1d9571baec7f5
KM Yim <32544571+kmyim@users.noreply.github.com>
2025-07-19 10:18:23 +0200

    added knn indexing comment to flexnbhd

--- skdim/_commonfuncs.py
+++ skdim/_commonfuncs.py
@@ -389,7 +389,7 @@ class FlexNbhdEstimator(BaseEstimator):
         smooth: if true, average over dimension estimates of local neighbourhoods using comb
         n_jobs: number of parallel processes in inferring local neighbourhood
         radius: radius parameter for nearest neighbour construction
-        n_neighbors: number of neighbors for k nearest neighbourhood construction
+        n_neighbors: number of neighbors for k nearest neighbourhood construction. Note we follow sklearn, where k = 1 means its own neighbourhood
         """
 
         self.pw_dim = pw_dim 
