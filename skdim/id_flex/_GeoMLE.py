02071145f6f4dd7020f1303f992cbdfad442767c
KM Yim <32544571+kmyim@users.noreply.github.com>
2025-07-19 10:17:56 +0200

    added knn indexing comment to flexnbhd

--- skdim/id_flex/_GeoMLE.py
+++ skdim/id_flex/_GeoMLE.py
@@ -5,12 +5,16 @@ from sklearn.metrics.pairwise import pairwise_distances
 import numpy as np
 
 class GeoMle(FlexNbhdEstimator):
-    def __init__(self, average_steps = 2, bootstrap_num = 20, alpha = 5e-3, interpolation_degree = 2,
+    def __init__(self, average_steps = 2, bootstrap_num = 20, bootstrap_frac = 0.5, alpha = 5e-3, interpolation_degree = 2,
         metric="euclidean",
         comb="mean",
         smooth=False,
         n_jobs=1,
-        n_neighbors=5):
+        n_neighbors=5,
+        random_state = 12345):
+        
+        self.effective_n_neighs = int((n_neighbors + average_steps) / bootstrap_frac) + 1  #neighbourhood size in building knn graph; inflated to ensure knn for mle in bootstrap samples
+
         super().__init__(
             pw_dim=True,
             nbhd_type="knn",
@@ -18,8 +22,9 @@ class GeoMle(FlexNbhdEstimator):
             comb=comb,
             smooth=smooth,
             n_jobs=n_jobs,
-            n_neighbors=n_neighbors,
-            sort_radial=False
+            n_neighbors= self.effective_n_neighs,
+            sort_radial=False, 
+            pt_nbhd_incl_pt=False
         )
         """
         Parameters
@@ -28,6 +33,8 @@ class GeoMle(FlexNbhdEstimator):
             Number of average steps. The default is 2. Estimator considers the k (number of neighbors) between k1 and k1 + average_steps - 1 (including).
         bootstrap_num : int, optional
             Number of bootstrap sets. The default is 20.
+        bootstrap_frac : float, optional
+            Fraction of points sampled for each bootstrap sample
         alpha : float, optional
             Regularization parameter for Ridge regression. The default is 5e-3.
         interpolation_degree : int, optional
@@ -39,22 +46,28 @@ class GeoMle(FlexNbhdEstimator):
         self.alpha = alpha
         self.max_degree = interpolation_degree
         self.bootstrap_num = bootstrap_num
+        self.boostrap_frac = bootstrap_frac
+        self.k2 = self.n_neighbors + self.average_steps - 1
+        self.random_state = random_state
 
     def _fit(self, X, nbhd_indices, radial_dists):
+        np.random.seed(self.random_state)
         # Check if the parameters are valid
-        if self.average_steps < 0:
-            raise ValueError("Number of average steps can not be negative")
-        if self.bootstrap_num <= 0:
+        if self.average_steps < 1 or not isinstance(self.average_steps, int):
+            raise ValueError("Number of average steps should be a positive integer > 1")
+        if not isinstance(self.boostrap_frac, float) or self.boostrap_frac < 1e-9 or self.boostrap_frac > 1-1e-9:
+            raise ValueError("bootstrap_frac is a float in (0,1) ")
+        if self.bootstrap_num <= 0 or not isinstance(self.bootstrap_num, int):
             raise ValueError("Number of bootstrap sets has to  be positive")
-        if self.max_degree <= 0:
-            raise ValueError("Degree of interpolation polynomial has to be positive")
-        if self.nbhd_type not in ['knn']:
-            raise ValueError('Neighbourhood type should be knn')
+        if self.max_degree <= 0 or not isinstance(self.bootstrap_num, int):
+            raise ValueError("Degree of interpolation polynomial has to be a positive integer ")
+        #if self.nbhd_type != 'knn':
+        #    raise ValueError('Neighbourhood type should be knn')
         
-        k2 = self.n_neighbors + self.average_steps - 1
-        self.dimension_pw_ = self.__geomle(X, self.n_neighbors, k2)
+        
+        self.dimension_pw_ = self.__geomle(X,  nbhd_indices, radial_dists)
     
-    def __geomle(self, X, k1, k2):
+    def __geomle(self, X,  nbhd_indices, radial_dists):
         """
         Returns range of Levina-Bickel dimensionality estimation for points in X averaged over bootstrap samples
     
@@ -68,18 +81,42 @@ class GeoMle(FlexNbhdEstimator):
         Returns: 
         array of shape (len(X),) of regression dimensionality estimation for points in X averaged over bootstrap samples
         """
-        if self.metric == 'precomputed':
-            dist = X
-            DIM_SPACE = None
-        else:
-            DIM_SPACE = X.shape[1]
-            dist = pairwise_distances(X, X, metric=self.metric)
-        NUM_OF_POINTS = X.shape[0]
+        k1 = self.k1
+        k2 = self.k2
+        bigk = np.shape(radial_dists)[1] #total number of neighbours per nbhd
+        
+        #### continue here 
+        ### parallelise this...
+        #randomly permute [True] * k2 + [False] self.effective_n_neighs - k2 for different rows 
+        # restrict radial_dist entries to those
+        rng = np.random.default_rng(self.random_state)
+        
+        #Handled by FlexNbhdEstimator 
+        #if self.metric == 'precomputed':
+        #    dist = X
+        #    DIM_SPACE = None
+        #else:
+        #    DIM_SPACE = X.shape[1]
+        #    dist = pairwise_distances(X, X, metric=self.metric)
+        #NUM_OF_POINTS = X.shape[0]
 
-        avg_distances = np.zeros((NUM_OF_POINTS , self.average_steps))
-        mles = np.zeros((NUM_OF_POINTS, self.bootstrap_num, self.average_steps))
-        final_mles = np.zeros(NUM_OF_POINTS)
+        #avg_distances = np.zeros((NUM_OF_POINTS , self.average_steps))
+        #mles = np.zeros((NUM_OF_POINTS, self.bootstrap_num, self.average_steps))
+        #final_mles = np.zeros(NUM_OF_POINTS)
+
+        mean_knn_radial_dist = None #store mean distance
+        mean_mle = None
+        var_mle = None
+        
         for j in range(self.bootstrap_num):
+            ## NEED TO CHECK WHETHER RADIAL_LIST CONTAINS ZERO IN FIRST COLUMN ##
+            subsampled_radial_dists = np.array([rd[rng.shuffle( [True] * k2 + [False]*(bigk - k2))] for rd in radial_dists]) #select radial distances, while keeping total order 
+            subsampled_mle = [[self._calc_mle(dlist[:r]) for r in range(k1, k2 + 1)] for dlist in subsampled_mle ]
+            if mean_knn_radial_dist == None:
+                mean_knn_radial_dist = subsampled_radial_dists /self.bootstrap_num
+            else:
+                mean_knn_radial_dist += subsampled_radial_dists /self.bootstrap_num
+
             bootstrap_ids = GeoMle.__gen_bootstrap_ids(X)
             # Calculate distances to nearest neighbours in bootsrap dataset
             for x_id in range(X.shape[0]):
