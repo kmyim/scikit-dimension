c430d92e8cafa2e79c7226f7155b0c6fc1ebe606
KM Yim <32544571+kmyim@users.noreply.github.com>
2025-09-24 16:25:51 +0100

    bug fixes and add global aggregation

--- skdim/id_flex/_GeoMLE.py
+++ skdim/id_flex/_GeoMLE.py
@@ -5,31 +5,23 @@ import numpy as np
 from joblib import Parallel, delayed
 
 class GeoMle(FlexNbhdEstimator):
-    def __init__(self, k1 = 5, k2= 7, bootstrap_num = 20, alpha = 5e-3, interpolation_degree = 2,
+    def __init__(self, k1 = 5, k2 = 10, bootstrap_nbhd = None, bootstrap_num = 20, alpha = 5e-3, interpolation_degree = 2, weight_reg= 1e-3,
         metric="euclidean",
         comb="mean",
         smooth=False,
         n_jobs=1,
-        random_state = 12345):
+        random_state = 12345,
+        localfit = True,
+        retain_curves = False):
         
-         #neighbourhood size in building knn graph; inflated to ensure knn for mle in bootstrap samples
-
-        super().__init__(
-            pw_dim=True,
-            nbhd_type="knn",
-            metric=metric,
-            comb=comb,
-            smooth=smooth,
-            n_jobs=n_jobs,
-            n_neighbors= self.k2,
-            sort_radial=False, 
-            pt_nbhd_incl_pt=False
-        )
+        
         """
         Implementation of GeoMLE [Gomtsyan19]. 
+        GeoMLE is a local MLE based estimator that uses bootstrapping and polynomial interpolation to reduce variance and bias of the basic Levina-Bickel MLE.
+        GeoMLE aggregates dimension estimates over a range of k-nearest neighbour sizes (k1,...,k2) using a polynomial regression fitted on bootstrap samples of the MLE estimates.
         We modify the original version so that the bootstrapping is done independently per point on an expanded knn neighbourhood, instead of the whole dataset. 
         This avoids repeated computation of distance matrices or KNN nbhds for each bootstrap sample of the whole dataset. 
-        Compared to implementation by authors, this issue fixes a bug involved with the bootstrapping procedure, that would confer a bias on the estimate
+        Compared to implementation by authors, we implement true bootstrapping (described in the true paper) as opposed to sub-sampling without replacement.
 
         Parameters
         ----------
@@ -43,38 +35,99 @@ class GeoMle(FlexNbhdEstimator):
             Regularization parameter for Ridge regression. The default is 5e-3.
         interpolation_degree : int, optional
             Degree of interpolation polynomial. The default is 2.
+        weight_reg: float, optional
+            weights on points in ridge regression are given by 1/(standard deviation in bootstrap ). The default is 0.5.
         random_state: int, optional
             Random seed for bootstrapping 
+        retain_curves: bool, optional
+            If True, retain the curves of MLE estimates, its std, and mean distances vs knn for each point and k. The default is False.
         """
         self.alpha = alpha
         self.max_degree = interpolation_degree
         self.bootstrap_num = bootstrap_num
+        self.localfit = localfit
+        
         self.k1 = k1
-        self.k2 = k2
+        self.k2 = k2 
+        
+        if bootstrap_nbhd is None:
+            self.bootstrap_nbhd = self.k2 + 5 #default neighbourhood for bootstrapping
+        else: 
+            self.bootstrap_nbhd = bootstrap_nbhd
+            
         self.random_state = random_state
+        self.weight_reg =weight_reg
+        self.retain_curves = retain_curves
+
+        super().__init__(
+            pw_dim=True,
+            nbhd_type="knn",
+            metric=metric,
+            comb=comb,
+            smooth=smooth,
+            n_jobs=n_jobs,
+            n_neighbors= self.bootstrap_nbhd,
+            sort_radial=False, 
+            pt_nbhd_incl_pt=False
+        )
 
     def _fit(self, X, nbhd_indices, radial_dists):
 
         # Check if the parameters are valid
-        for idx, p in enumerate([self.k1, self.k2]):
-            if not isinstance(p, int) or p < 3:
-                raise ValueError("k" + str(idx+1) + " should be a positive integer at least 3.")
-        if self.k1 >= self.k2:
-            raise ValueError("k2 needs to be strictly greater than k1.")   
-        if self.bootstrap_num <= 0 or not isinstance(self.bootstrap_num, int):
-            raise ValueError("Number of bootstrap sets needs to be a positive integer")
+        if not isinstance(self.k1, int) or self.k1 < 3:
+            raise ValueError("k1 should be a positive integer at least 3.")
+        if self.k1 >= self.k2 or not isinstance(self.k2, int) or  self.k1 < 3:
+            raise ValueError("steps needs to be strictly positive integer.")   
+        if self.bootstrap_nbhd < self.k2 or not isinstance(self.bootstrap_nbhd, int) or  self.bootstrap_nbhd < 3:
+            raise ValueError("Bootstrap neighbourhood must be greater than k1 + steps.")  
+        if self.bootstrap_num < 0 or not isinstance(self.bootstrap_num, int):
+            raise ValueError("Number of bootstrap sets needs to be a non-negative integer.")
         if self.max_degree <= 0 or not isinstance(self.max_degree, int):
-            raise ValueError("Degree of interpolation polynomial has to be a positive integer ")
-
+            raise ValueError("Degree of interpolation polynomial has to be a positive integer.")
+        if not isinstance(self.localfit, bool):
+            raise ValueError("Local fit must be boolean.")
         np.random.seed(self.random_state)
 
         if isinstance(self.n_jobs, int) and not self.n_jobs in [0,1]:
-            with Parallel(n_jobs=self.n_jobs) as parallel:
-                 self.dimension_pw_ = parallel(
-                    delayed(self.__local_geomle)(r)
-                    for r in radial_dists)
+            if self.localfit:
+                with Parallel(n_jobs=self.n_jobs) as parallel:
+                    res = parallel(
+                        delayed(self.__local_geomle)(r)
+                        for r in radial_dists)
+            else:
+                with Parallel(n_jobs=self.n_jobs) as parallel:
+                    res = parallel(
+                        delayed(self._calc_local_mle_all_ks_)(r, self.k1, self.k2)
+                        for r in radial_dists)
         else:
-            self.dimension_pw_ = np.array([self.__local_geomle(r) for r in radial_dists])
+            if self.localfit:
+                res = [self.__local_geomle(r) for r in radial_dists]
+            else:
+                res = [self._calc_local_mle_all_ks(r, self.k1, self.k2) for r in radial_dists]
+        
+        if self.localfit:
+            self.dimension_pw_ = np.array([a[0] for a in res])
+            if self.retain_curves:
+                self.mle_k_pw = np.array([a[1] for a in res])
+                self.mle_k_var = np.array([a[2] for a in res])
+                self.mean_dk = np.array([a[3] for a in res])
+            else:
+                self.mle_k_pw = None
+                self.mle_k_var = None
+                self.mean_dk = None
+        else:
+            self.dimension_pw_ = res
+            dists = np.array([r for row in radial_dists[:,self.k1-1:self.k2] for r in row])
+            X = np.vander(dists, self.max_degree + 1, increasing = True)[:,1:] 
+            y = np.array([r for row in self.dimension_pw_ for r in row])
+            ridge_reg = Ridge(alpha=self.alpha, fit_intercept=True)
+            ridge_reg.fit(X, y)
+            self.dimension_global_ = ridge_reg.intercept_
+            if self.retain_curves:
+                self.dists_ = dists
+                self.regre_coef_ = ridge_reg.coef_
+
+
     
     def __local_geomle(self, radial_dists):
         """    
@@ -88,37 +141,40 @@ class GeoMle(FlexNbhdEstimator):
         mean_knn_radial_dist = None #store mean distance
         mean_mle = None #store mean mle estimate
         var_mle = None #store var in mle estimate
-        
-        for _ in range(self.bootstrap_num):
-            ### parallelise this...
-            ## row of radial_list = [d1 ( > 0), d2,...]  ##
-            btstrp_radial_dists = self._bootstrap_order_preserving(radial_dists) #bootstrap resample of NN distances while keeping total order in array
-            btstrp_mle = self._calc_local_mle_all_ks(btstrp_radial_dists, self.k1, self.k2) # mle estimates for nbhds of size k1,...,k2
+        if self.bootstrap_num > 0:
+            for _ in range(self.bootstrap_num):
+                ### parallelise this...
+                ## row of radial_list = [d1 ( > 0), d2,...]  ##
+                btstrp_radial_dists = self._bootstrap_order_preserving(radial_dists) #bootstrap resample of NN distances while keeping total order in array
+                btstrp_mle = self._calc_local_mle_all_ks(btstrp_radial_dists, self.k1, self.k2) # mle estimates for nbhds of size k1,...,k2
 
-            if mean_knn_radial_dist is None: #initialise
-                mean_knn_radial_dist = btstrp_radial_dists /self.bootstrap_num # length (k2- k1+ 1)
-            else:
-                mean_knn_radial_dist += btstrp_radial_dists /self.bootstrap_num #update
+                if mean_knn_radial_dist is None: #initialise
+                    mean_knn_radial_dist = btstrp_radial_dists /self.bootstrap_num # length k2
+                else:
+                    mean_knn_radial_dist += btstrp_radial_dists /self.bootstrap_num #update
 
-            if mean_mle is None:
-                mean_mle = btstrp_mle / self.bootstrap_num # length (k2- k1+ 1)
-            else:
-                mean_mle += btstrp_mle / self.bootstrap_num
-            
-            if var_mle is None:
-                var_mle = btstrp_mle ** 2/ self.bootstrap_num
-            else:
-                var_mle += btstrp_mle ** 2/ self.bootstrap_num
+                if mean_mle is None:
+                    mean_mle = btstrp_mle / self.bootstrap_num # length (k2- k1+ 1)
+                else:
+                    mean_mle += btstrp_mle / self.bootstrap_num
+                
+                if var_mle is None:
+                    var_mle = btstrp_mle ** 2/ self.bootstrap_num
+                else:
+                    var_mle += btstrp_mle ** 2/ self.bootstrap_num
+            var_mle -=  mean_mle **2 #length (k2- k 1+ 1) subtract mean**2 to get variance from mean(X**2)
+        else:
+            mean_knn_radial_dist = radial_dists # length k2
+            mean_mle = self._calc_local_mle_all_ks(radial_dists, self.k1, self.k2) # length (k2- k1+ 1)
+            var_mle = np.ones_like(mean_mle) # no variance information
         
-        var_mle -=  mean_mle **2 #length (k2- k 1+ 1) subtract mean**2 to get variance from mean(X**2)
-        return self._calc_local_estimate_from_regression(mean_mle, var_mle, mean_knn_radial_dist)
+        
+        return self._calc_local_estimate_from_regression(mean_mle, var_mle, mean_knn_radial_dist[self.k1-1:self.k2]), mean_mle, var_mle, mean_knn_radial_dist[self.k1-1:self.k2]
 
     def _calc_local_estimate_from_regression(self, mean_mle, var_mle, mean_knn_radial_dist):
         # per point! not over all
-        ### TO DO NICE POLYNOMIAL BASIS FOR BETTER NUMERICS? 
-
-        weights = np.divide(1, var_mle)
-        X = np.power(mean_knn_radial_dist.reshape([-1,1]), np.arange(1, self.max_degree+ 1)) # extrapolation points x features; row = ith distance, column the power; 
+        weights = np.divide(1, np.maximum(np.sqrt(var_mle),self.weight_reg))
+        X = np.vander(mean_knn_radial_dist, self.max_degree + 1, increasing = True)[:,1:] # extrapolation points x features; row = ith distance, column the power;
         ridge_reg = Ridge(alpha=self.alpha, fit_intercept=True)
         ridge_reg.fit(X, mean_mle, weights)
         return ridge_reg.intercept_
@@ -130,21 +186,22 @@ class GeoMle(FlexNbhdEstimator):
         filt = [0]*len(a)
         for i in idx:
             filt[i]+=1
-
         return np.repeat(a, filt)
         
 
     @staticmethod
     def _calc_local_mle_all_ks(dlist, k1, k2):
-        #assume dlist sorted in increasing order, which is true for knn
+        # compute mle estimates for k = k1,...,k2
+        # assume dlist sorted in increasing order, which is true for knn
         #(i-1)th entry in dlist is the ith nearest distinct neighbour
         #j-1 entry in cumsum = sum(log(d_i), i = 1,..., j )
-        logsum = np.cumsum(np.log(dlist))[k1-1:k2] # = [sum(log(d_i, i = 1,...,k1), ..., sum(log(d_i, i = 1,...,k2)]. length #k2- k1 + 1
-        lograd = np.arange(k1, k2 + 1) * np.log(dlist[k1-1:k2]) # for each k in k1,...k2, compute k*log(d_k)
+        logsum = np.cumsum(np.log(dlist))[k1-2:k2-1] # = [sum(log(d_i, i = 1,...,k1-1), ..., sum(log(d_i, i = 1,...,k2-1)]. length #k2- k1 + 1
+        lograd = np.arange(k1-1, k2) * np.log(dlist[k1-1:k2]) # for each k in k1,...k2, compute (k-1)*log(d_k)
         allminv = lograd - logsum
+        if np.any(allminv < 0):
+            raise ValueError("MLE estimate is ill-define due to non-distinct nearest neighbours. Try increasing k1 or bootstrap_nbhd parameters.")
         #returns mle estimates for k1,...,k2
         return np.divide(np.arange(k1-2, k2-1), allminv) # (k1-2,..., k2-2) / allminv; use k-2 (instead k-1) to get rid of the asymptotic bias
 
 
 
-#### TO DO ####
